63c11b24f7f6b4d1f2f5378fb383cdfec99ff10c
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-05 18:08:44 +0530

    added styles material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import moments from "./images/moments.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+
 require("dotenv").config();
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>{process.env.REACT_APP_URL}</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Moments
+        </Typography>
+        <img className={} src={moments} alt="moments" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post/Post";
+
+const Posts = () => {
+  return (
+    <>
+      <Post />
+      <Post />
+      <h1>POSTS</h1>
+    </>
+  );
+};
+
+export default Posts;
