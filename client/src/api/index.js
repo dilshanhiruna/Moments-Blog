a29b9531882c1ba73f3d68cf37e9557277a4e1f6
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-06 18:58:47 +0530

    form dispatch

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,3 +2,4 @@ import axios from "axios";
 
 const url = process.env.REACT_APP_URL + "posts";
 export const fetchPosts = () => axios.get(url);
+export const createPost =(newPost)=>{axios.post(URL,newPost)}
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts";
 
 const Form = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   const [postData, setpostData] = useState({
     creator: "",
@@ -14,7 +17,10 @@ const Form = () => {
     selectorFile: "",
   });
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPost(postData));
+  };
 
   const clear = () => {};
 
