e8b12aef6d3c0627d9711d73ccc3dbea351c9b8c
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-06 18:04:47 +0530

    implemented getPost action

--- server/index.js
+++ server/index.js
@@ -9,11 +9,10 @@ dotenv.config();
 
 const app = express();
 
-app.use("/post", PostRouter);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/post", PostRouter);
 
 const URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 5000;
