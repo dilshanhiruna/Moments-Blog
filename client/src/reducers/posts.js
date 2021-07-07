4ec0dad3b1d9bac5cd73134fdde0925087920820
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-07 18:19:16 +0530

    Prepared for deploy

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,17 +1,25 @@
+import {
+  CREATE,
+  UPDATE,
+  DELETE,
+  FETCH_ALL,
+  LIKE,
+} from "../constants/actionTypes";
+
 const post = (posts = [], action) => {
   switch (action.type) {
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
 
-    case "UPDATE":
-    case "LIKE":
+    case UPDATE:
+    case LIKE:
       return posts.map((post) =>
         post._id === action.payload ? action.payload : post
       );
 
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
 
     default:
