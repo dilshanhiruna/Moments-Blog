9711570ff0bcc0fe8dc0d0e2031a921c8d93f6d7
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-07-07 09:15:48 +0530

    Added material UI to all Post container

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,9 +1,25 @@
 import React from "react";
 import useStyles from "./style";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAl";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHiriIcon from "@material-ui/icons/MoreHoriz";
 
 const Post = () => {
   const classes = useStyles();
-  return <h1>POST</h1>;
+  return(
+    <Card className={classes.card}>
+      <CardMedia
+
+    </Card>
+  );
 };
 
 export default Post;
